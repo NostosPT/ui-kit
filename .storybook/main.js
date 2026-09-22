eae710d705280988a37b7089cc31c203eb5b990a
Tiago Oliveira <tiagoco2008@gmail.com>
2026-09-22 20:24:08 +0100

    Enable Svelte CSF stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,11 @@
 /** @type {import('@storybook/svelte-vite').StorybookConfig} */
 export default {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|svelte)"],
-  addons: ["@storybook/addon-docs", "@storybook/addon-themes"],
+  addons: [
+    "@storybook/addon-docs",
+    "@storybook/addon-themes",
+    "@storybook/addon-svelte-csf"
+  ],
   framework: { name: "@storybook/svelte-vite", options: {} },
   staticDirs: ["./public"]
 };
